2afa03557bd3880faad493665c12e4ed08bbd72c
Josh Mulloy <josh@mulloy.ca>
2021-09-01 01:02:58 -0400

    Updated Structure

--- albums-public/src/components/AddAlbum.js
+++ albums-public/src/components/AddAlbum.js
@@ -1,155 +1,26 @@
-import React, { Component } from 'react';
-import AlbumDataService from '../services/album.service';
+import React from 'react';
+import { connect } from 'react-redux';
 
-class AddAlbum extends Component {
-    constructor(props) {
-        super(props);
-        this.onChangeTitle = this.onChangeTitle.bind(this);
-        this.onChangeArtist = this.onChangeArtist.bind(this);
-        this.onChangeSpotifyLink = this.onChangeSpotifyLink.bind(this);
-        this.onChangeImageUrl = this.onChangeImageUrl.bind(this);
-        this.saveAlbum = this.saveAlbum.bind(this);
-        this.newAlbum = this.newAlbum.bind(this);
-        
-        this.state = {
-            title: "",
-            artist: "",
-            spotifyLink: "",
-            imageUrl: "",
-            submitted: false,
-        }
-    }
+const AddAlbum = (props) => {
+    const {
+        REACT_APP_CLIENT_ID,
+        REACT_APP_AUTHORIZE_URL,
+        REACT_APP_REDIRECT_URL
+    } = process.env;
 
-    onChangeTitle(e) {
-        this.setState({
-            title: e.target.value,
-        });
-    }
+    const handleLogin = () => {
+        window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+    };
 
-    onChangeArtist(e) {
-        this.setState({
-            artist: e.target.value,
-        });
-    }
+    const { isValidSession, location } = props;
+    const { state } = location;
+    const sessionExpired = state && state.session_expired;
 
-    onChangeSpotifyLink(e) {
-        this.setState({
-            spotifyLink: e.target.value,
-        });
-    }
+    return (
+        <div>
+            Spotify
+        </div>
+    );
+};
 
-    onChangeImageUrl(e) {
-        this.setState({
-            imageUrl: e.target.value,
-        });
-    }
-
-    saveAlbum() {
-        var album = {
-            title: this.state.title,
-            artist: this.state.artist,
-            spotifyLink: this.state.spotifyLink,
-            imageUrl: this.state.imageUrl
-        }
-
-        AlbumDataService.create(album)
-            .then(res => {
-                this.setState({
-                    title: res.data.title,
-                    artist: res.data.artist,
-                    spotifyLink: res.data.spotifyLink,
-                    imageUrl: res.data.imageUrl,
-                    
-                    submitted: true,
-                });
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
-
-    newAlbum() {
-        this.setState({
-            title: "",
-            artist: "",
-            spotifyLink: "",
-            imageUrl: "",
-            submitted: false,
-        })
-    }
-    
-    render() { 
-        return ( 
-            <div className="submit-form">
-                {this.state.submitted ? (
-                    <div>
-                        <h4>You submitted successfully!</h4>
-                        <button className="btn btn-success" onClick={this.newAlbum}>
-                            Add
-                        </button>
-                    </div>
-                ) : (
-                    <div>
-                        <div className="form-group">
-                            <label htmlFor="title">Title</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="title"
-                                required
-                                value={this.state.title}
-                                onChange={this.onChangeTitle}
-                                name="title"
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="artist">Artist</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="artist"
-                                required
-                                value={this.state.artist}
-                                onChange={this.onChangeArtist}
-                                name="artist"
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="spotifyUrl">Spotify Link</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="spotifyUrl"
-                                required
-                                value={this.state.spotifyLink}
-                                onChange={this.onChangeSpotifyLink}
-                                name="spotifyUrl"
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="imageUrl">Image Url</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="imageUrl"
-                                required
-                                value={this.state.imageUrl}
-                                onChange={this.onChangeImageUrl}
-                                name="imageUrl"
-                            />
-                        </div>
-
-                        <button onClick={this.saveAlbum} className="btn btn-success">
-                            Submit
-                        </button>
-                    </div>
-                )}
-            </div>
-         );
-    }
-}
- 
-export default AddAlbum;
+export default connect()(AddAlbum);
